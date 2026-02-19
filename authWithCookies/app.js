2022f6ea07e0ee088c5cacc2933f39b6186f4488
jiocode-edu <ifmdac80@gmail.com>
2026-02-19 17:31:32 +0530

    19-02-2026

---
+++ authWithCookies/app.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import cookieParser from 'cookie-parser'
+
+
+dotenv.config({
+  path:'./.env'
+})
+
+const app = express()
+
+const port = process.env.PORT
+
+mongoose.connect(process.env.MONGODB_URL).then(() => console.log('🍔MongoDB Connected')).catch((err) => console.error('MongoDB Error' , err))
+
+app.set('view engine' , 'ejs')
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cookieParser('lecture-auth-2026'))
+
+app.listen(port , () => {
+  console.log(`server start on port ${port}`);
+})
---
+++ authWithCookies/redmi.md
@@ -0,0 +1,15 @@
+# INTRO AND UNDERSTANDING PARTIAL VIEWSLECTURE
+
+# Understand layouts creating layout Setting up static files
+
+# INTRODUCTION TO MANUAL AUTH USING COOKIELECTURE
+	
+# Introduction of Cookies Setting up Cookie ( Store & Retrieve )
+	
+# INTRODUCTION TO MANUAL AUTH USING COOKIELECTURE
+
+# Make Signup, SignIn using Cookie
+
+# INTRODUCTION TO MANUAL AUTH USING COOKIELECTURE
+	
+# setup security using Cookie Using Cookie Restrict the page
